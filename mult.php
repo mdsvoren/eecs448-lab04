d19e0243a8b63141f711669b01c97598bcc68693
Michael Svoren <msvoren@ku.edu>
2019-02-28 13:48:46 -0600

    fuctional table

--- mult.php
+++ mult.php
@@ -1,1 +1,23 @@
-echo "Hello, World!";
+ <?php
+
+ function printTable($rows, $cols)
+ {
+   echo "<table border='1'>";
+   for ($i = 1; $i<=$rows; $i++)
+   {
+     echo "<tr>";
+     for ($j = 1; $j<=$cols; $j++)
+     {
+       $product = $i * $j;
+       echo "<td>";
+       echo $product;
+       echo "</td>";
+     }
+     echo "</tr>";
+   }
+   echo "</table>";
+ }
+
+ printTable(100,100);
+
+?>
