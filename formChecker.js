0d06b3c24e6e91d64c15180515d6c2a8c584e56e
Michael Svoren <michael.svoren@gmail.com>
2019-03-04 18:07:02 -0600

    testing in js

--- formChecker.js
+++ formChecker.js
@@ -1,8 +1,69 @@
 function testFunc()
 {
-  // elatoQ = document.getElementById("q1");
-  //console.log(elatoQ);
-  console.log("After");
+  let gelatoQ = quantityTest(document.getElementById("gelato").value);
+  let sigfoxQ = quantityTest(document.getElementById("sigfox").value);
+  let bookQ = quantityTest(document.getElementById("book").value);
 
-  //use preventDefault()
+  let shipMethod = checkBox(getRadioVal());
+
+  let email = ValidateMail(document.getElementById("userName").value);
+  let password = document.getElementById("passwordText").value;
+
+  
+
+
+  function getRadioVal()
+   {
+    let val;
+    // get list of radio buttons with specified name
+    let buttons = document.getElementsByName("shipping");
+    
+    // go through radio buttons to see if 
+    for (let i=0, len=buttons.length; i<len; i++) 
+    {
+        if (buttons[i].checked)
+        {
+          val = buttons[i].value; 
+          break;
+        }
+    }
+    return (val);
+  }
+
+  
+ //use preventDefault()
+}
+
+function quantityTest(quantity)
+{
+  if (Number.isInteger(quantity) && quantity >=0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+function checkBox(val)
+{
+  if (val!= undefined)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//this function was taken from https://www.w3resource.com/javascript/form/email-validation.php
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    return (false)
 }
